20ca1d26857a557fc4fb8970dbd4fd437840a9a9
Corey Quillen <corey.quillen@gmail.com>
2015-01-24 01:17:44 -0600

    configure email to use sendgrid

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '4.2.0'
 gem 'sqlite3', group: :development
 gem 'pg', group: :production
 gem 'rails_12factor', group: :production
+gem 'figaro'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -20,11 +20,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              'smtp.sendgrid.com',
     port:                 587,
-    domain:               'localhost',
-    user_name:            '',
-    password:             '',
+    domain:               'servicewise.co',
+    user_name:            ENV['SENDGRID_USERNAME'],
+    password:             ENV['SENDGRID_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true
   }
